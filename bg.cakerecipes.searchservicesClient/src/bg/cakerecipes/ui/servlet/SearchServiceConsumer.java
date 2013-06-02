d16a6012adecf324220cee31fa10fc320a4e3a79
leni-kirilov <leni.kirilov@gmail.com>
2013-06-02 04:43:31 +0300

    Queried rank and IDs Map is sorted in TestServlet

--- bg.cakerecipes.searchservicesClient/src/bg/cakerecipes/ui/servlet/SearchServiceConsumer.java
+++ bg.cakerecipes.searchservicesClient/src/bg/cakerecipes/ui/servlet/SearchServiceConsumer.java
@@ -2,6 +2,10 @@ package bg.cakerecipes.ui.servlet;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -36,11 +40,36 @@ public class SearchServiceConsumer {
 	 */
 	private void displayRankingChart(Map<String, Long> rankingMap, PrintStream out){
 		
-		//TODO display this chart sorted by rank (highest first) - may be this http://stackoverflow.com/questions/7965132/java-sort-hashmap-by-value ... but looks like overkill
-		for (String objectId : rankingMap.keySet()) {
-			out.println("<br>object-id= " + objectId + " -->");
-			out.println("object-rank= " + rankingMap.get(objectId));
+		Map<String, Long> sortedMap = sortByValue(rankingMap);
+		
+		for (String objectId : sortedMap.keySet()) {
+			out.println("<br>object-rank= " + rankingMap.get(objectId));
+			out.println("object-id= " + objectId + " -->");
 			out.println("<br>----------"); 
 		}
 	}
+
+	/**
+	 * Very cool algorithm from here: http://stackoverflow.com/questions/7965132/java-sort-hashmap-by-value
+	 * 
+	 * @param map
+	 * @return sorted by value LinkedHashMap
+	 */
+	//TODO move to Query service may be?
+	private static Map<String, Long> sortByValue(Map<String, Long> map) {
+        List<Map.Entry<String, Long>> list = new LinkedList<Map.Entry<String, Long>>(map.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
+
+            public int compare(Map.Entry<String, Long> m1, Map.Entry<String, Long> m2) {
+                return (m2.getValue()).compareTo(m1.getValue());
+            }
+        });
+
+        Map<String, Long> result = new LinkedHashMap<String, Long>();
+        for (Map.Entry<String, Long> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
 }
